2b8c435313f2e0a2faa6a22d2d3916a8f1625ee3
John <jleeggbuddy@gmail.com>
2019-01-11 06:58:46 +1300

    yolo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { map, filter, debounceTime, switchMap } from 'rxjs/operators';
+import { CountryService } from './country.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tm-rxjs-mini-workshop';
+export class AppComponent implements OnInit {
+
+  public suggetions$: Observable<string[]>;
+
+  constructor(private countryService: CountryService) { }
+
+  ngOnInit() {
+    const searchBox = document.getElementById('search-box');
+
+    this.suggetions$ = fromEvent(searchBox, 'input').pipe(
+      map((e: KeyboardEvent) => (e.target as any).value),
+      filter(text => text.length > 2),
+      debounceTime(500),
+      switchMap((text) => this.countryService.getCountries(text))
+    );
+  }
+
 }
